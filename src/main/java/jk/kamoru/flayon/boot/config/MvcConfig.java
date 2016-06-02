177c872a369934aa46c263cfdd0166cf55abb5fe
kamoru <kamoru@kamoru-mint>
2016-06-02 20:53:57 +0900

    add ContentNegotiatingViewResolver, angularJS

--- src/main/java/jk/kamoru/flayon/boot/config/MvcConfig.java
+++ src/main/java/jk/kamoru/flayon/boot/config/MvcConfig.java
@@ -1,15 +1,24 @@
 package jk.kamoru.flayon.boot.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import jk.kamoru.flayon.boot.aop.HandlerAccessLogger;
 import jk.kamoru.flayon.boot.aop.HandlerAccessLogger.WHEN;
@@ -36,7 +45,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		localeChangeInterceptor.setParamName("lang");
 		return localeChangeInterceptor;
 	}
-	 
 
 	@Bean(name = "localeResolver")
 	public LocaleResolver sessionLocaleResolver() {
@@ -49,4 +57,30 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.TEXT_HTML);
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		resolvers.add(new ViewResolver() {
+
+			@Override
+			public View resolveViewName(String viewName, Locale locale) throws Exception {
+				MappingJackson2JsonView view = new MappingJackson2JsonView();
+				view.setPrettyPrint(true); 
+				return view;
+			}
+			
+		});
+
+	    ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+	    resolver.setViewResolvers(resolvers);
+	    resolver.setContentNegotiationManager(manager);
+	    return resolver;
+	  }
 }
