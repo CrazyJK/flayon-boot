4802efbaf943f23f6c73069c670f9f778460c080
kamoru <kamoru@kamoru-home>
2016-10-15 19:55:06 +0900

    Registered user modifications

--- src/main/java/jk/kamoru/flayon/boot/beans/MethodExecutionSample.java
+++ src/main/java/jk/kamoru/flayon/boot/beans/MethodExecutionSample.java
@@ -5,21 +5,17 @@ import org.springframework.stereotype.Component;
 
 import jk.kamoru.flayon.boot.security.User;
 import jk.kamoru.flayon.boot.security.UserRepository;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
-@Slf4j
 public class MethodExecutionSample {
 
 	@Autowired private UserRepository userRepository;
 
 	public void loadInitData() {
 		User user = new User();
-		user.setName("kamoru" + Double.valueOf(Math.random() * 100).intValue());
-		user.setPassword(String.valueOf(Double.valueOf(Math.random() * 100).intValue()));
+		user.setName("kamoru");
+		user.setPassword("crazyjk");
 		user.setRole("USER");
 		userRepository.save(user);
-		
-		log.info("set {}", user);
 	}
 }
